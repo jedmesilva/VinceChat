32a0eedada0813dca471340316c782272db1d873
Jedme Silva <157550741+jedmesilva@users.noreply.github.com>
2025-07-18 20:07:16 +0000

    Refactory

--- client/src/pages/my-vault.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MyVaultMain from '@/components/MyVault/MyVaultMain';
-
-const MyVaultPage: React.FC = () => {
-  return <MyVaultMain />;
-};
-
-export default MyVaultPage;
